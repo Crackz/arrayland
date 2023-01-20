cdf1924ffced51b40b8af8d2d7ab0f1cce6a6c47
Mohamed Magdy <vipa00magdy00@gmail.com>
2023-01-21 01:06:53 +0200

    Remove Unused Global CSS From SVG Viewer

--- src/components/svg-viewer.component.tsx
+++ src/components/svg-viewer.component.tsx
@@ -1,9 +1,18 @@
+import { Box, styled } from '@mui/material';
 import Image from 'next/image';
 import exampleSVG from 'public/example.svg';
 
+const SVGFrame = styled(Box)(() => ({
+    flex: 1,
+    minWidth: '50%',
+    border: '5px solid #393E46',
+    resize: 'both',
+    overflow: 'auto',
+}));
+
 const SVGViewer = () => {
     return (
-        <div id="imageArea">
+        <SVGFrame display="flex">
             <Image
                 id="image"
                 src={exampleSVG}
@@ -13,7 +22,7 @@ const SVGViewer = () => {
                 sizes="100vw"
                 style={{ width: '100%', height: '100%' }}
             />
-        </div>
+        </SVGFrame>
     );
 };
 
