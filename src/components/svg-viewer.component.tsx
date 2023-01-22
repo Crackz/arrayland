58133adbab9c9d926c7314caf3fb22dc59bad006
Mohamed Magdy <vipa00magdy00@gmail.com>
2023-01-22 06:48:46 +0200

    Gray out SVG Viewer On Loading

--- src/components/svg-viewer.component.tsx
+++ src/components/svg-viewer.component.tsx
@@ -1,28 +1,42 @@
-import { Box, styled } from '@mui/material';
+import { Grid } from '@mui/material';
 import Image from 'next/image';
 import exampleSVG from 'public/example.svg';
 
-const SVGFrame = styled(Box)(() => ({
-    flex: 1,
-    minWidth: '50%',
-    border: '5px solid #393E46',
-    resize: 'both',
-    overflow: 'auto',
-}));
+const SVGViewer = ({ isLoading }: { isLoading: boolean }) => {
+    let style = {};
+    if (isLoading) {
+        style = {
+            opacity: 0.4,
+            filter: 'alpha(opacity=40)',
+            backgroundColor: '#000',
+        };
+    }
 
-const SVGViewer = () => {
     return (
-        <SVGFrame display="flex">
+        <Grid
+            container
+            border="5px solid #393E46"
+            justifyContent="center"
+            textAlign="center"
+            sx={{
+                resize: 'both',
+                overflow: 'auto',
+            }}
+        >
             <Image
+                priority
                 id="image"
                 src={exampleSVG}
                 alt={''}
                 width={0}
                 height={0}
-                sizes="100vw"
-                style={{ width: '100%', height: '100%' }}
+                style={{
+                    ...style,
+                    width: '100%',
+                    height: '100%',
+                }}
             />
-        </SVGFrame>
+        </Grid>
     );
 };
 
